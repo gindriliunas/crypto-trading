0cd19e1034cd8ecec5ee967110b61a5796e55cc8
Gintas <g.indriliunas@gmail.com>
2026-08-15 22:25:04 +0100

    update terrafom

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
+resource "aws_s3_bucket" "terraformstatefolder" {
+  bucket = "terraformstatefolder-631026310596"
+}
+terraform {
+  backend "s3" {
+    bucket         = "terraformstatefolder-631026310596"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
+}
